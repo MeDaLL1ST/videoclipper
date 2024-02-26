53782a2b4d468dddc73c2c7e5cfbbe858e0ee59e
KORABLEV <63864439+MeDaLL1ST@users.noreply.github.com>
2024-02-26 19:49:50 +0300

    commit

--- main.go
+++ main.go
@@ -223,7 +223,7 @@ func newPool() *redis.Pool {
 		MaxIdle: 80,
 		MaxActive: 12000,
 		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", "188.225.73.124:6379", redis.DialPassword("82js1khpi8"))
+			c, err := redis.Dial("tcp", ":6379", redis.DialPassword(""))
 
 			if err != nil {
 				panic(err.Error())
